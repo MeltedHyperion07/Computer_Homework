9d16234ee10930d95ae508fbc4824754d4126c0f
Arjun Gupta <arjungupta2908@gmail.com>
2020-05-15 20:22:18 +0530

    Init

--- src/Question1_PascalTriangle.java
+++ src/Question1_PascalTriangle.java
@@ -1,2 +1,16 @@
+import java.util.*;
+
 public class Question1_PascalTriangle {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 1; i <= n; i++) {
+            int C = 1;
+            for (int j = 1; j <= i; j++) {
+                System.out.print(C + " ");
+                C = C * (i - j) / j;
+            }
+            System.out.println();
+        }
+    }
 }
